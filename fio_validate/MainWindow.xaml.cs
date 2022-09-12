56aa184211fd1c4be49b131d10b4f2aa92cb40b2
dron2309 <andrei03jump@gmail.com>
2022-09-13 00:51:18 +0300

    first commit

---
+++ fio_validate/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace fio_validate
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private User _user;
+        private ObservableCollection<string> list;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _user = new User();
+            list = new ObservableCollection<string> { };
+        }
+
+        private void vnos_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+            _user.otchestvo1 = otchestvo.Text;
+            _user.familiya1 = familiya.Text;
+            _user.imya1 = imya.Text;
+            _user.FIO = (imya.Text + " " + familiya.Text + " " + otchestvo.Text);
+            list.Add(_user.FIO);
+            list1.ItemsSource = list;
+
+        }
+    }
+}
---
+++ fio_validate/user.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fio_validate
+{
+     class User
+    {
+        public string otchestvo1 { get; set; }
+        public string familiya1 { get; set; }
+        public string imya1 { get; set; }
+        public string FIO { get; set; }
+    }
+}
